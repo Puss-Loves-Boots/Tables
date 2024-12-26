3678b1f862d7efc302458b184f6f07ad4d553037
lzy <3468154022@qq.com>
2024-12-26 17:01:08 +0800

    全部四张表的十六进制码表已经生成完毕，至此所有理论值已经梳理完毕。 等待现场实际机箱的分区编号最终形成实际控制码表

--- tables/ctrl.py
+++ tables/ctrl.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_excel('./变频天线阵.xlsx', sheet_name="支路A改", dtype=str)
+data = pd.read_excel('./何老师发的 excel/天线阵-四张表.xlsx', sheet_name="Sheet3", dtype=str)
 print(data.shape)
 
 hex2_list = []
@@ -8,13 +8,13 @@ hex2_list = []
 for row in data.itertuples(index=False):
     # print(row)
     if type(row[1]) == str:
-        col = row[0] + row[1] + row[2] + row[3] + row[4]
+        col = row[0] + row[1] + row[2]
         dec = int(col, 2)
-        hex2 = '0x{:06x}'.format(dec).upper()
+        hex2 = '0x{:04x}'.format(dec).upper()
         hex2_list.append(hex2)  
     else:
         hex2_list.append('')  
 
 data['hex'] = hex2_list
 
-data.to_excel('./变频天线阵_hex.xlsx', sheet_name="支路A改", index=False)
+data.to_excel('./何老师发的 excel/1.xlsx', sheet_name="sheet3", index=False)
